311421f6218d0a84452b8b631d48d10bd3ba2c54
Frank Manno <frank@aquabox.ca>
2024-11-04 13:57:44 -0500

    check if initial selector is a paragraph; cleaned up comments

--- public/diy-onic-converter.js
+++ public/diy-onic-converter.js
@@ -4,28 +4,35 @@ const BIONIC_WORD_LIMIT = 3;
 const diyOnicConverter = (textContentContainerSelector = 'body') => {
   const container = document.querySelector(textContentContainerSelector);
 
-  // let's find all the paragraph nodes within our selector
-  const paragraphs = container.querySelectorAll('p');
+  // if our selector is a single paragraph, we can skip the
+  // querySelectorAll query below, and just use a single element
+  // array of our `container` to loop over.
+  const isContainerAParagraph = container.nodeName.toLowerCase() === 'p';
+
+  console.log('Performing bionic reading conversion on:', container);
+
+  // Let's find all the paragraph nodes within our container
+  // (if it's a non-paragraph); otherwise, we operate directly
+  // on the container.
+  const paragraphs = isContainerAParagraph
+    ? [container]
+    : container.querySelectorAll('p');
 
   for (const paragraph of paragraphs) {
-    console.log(paragraph);
-
-    // split the paragraph into its separate words, so we can iterate and perform
-    // our bionic process
+    // Split the paragraph into its separate words, so we
+    // can iterate and perform our bionic formatting.
     const words = paragraph.innerText.split(' ');
     const bionicWords = words.map(word => {
       const leading = `<strong>${word.slice(0, BIONIC_WORD_LIMIT)}</strong>`;
       const trailing = word.slice(BIONIC_WORD_LIMIT);
 
-      // return a bolded representation
+      // Bionic-formatted word
       return `${leading}${trailing}`;
     });
 
     // Replace the original paragraph with the joined bionicWords
     paragraph.innerHTML = bionicWords.join(' ');
   }
-
-  console.log(paragraphs);
 };
 
 // Allow global access so that this can be executed from the console.
